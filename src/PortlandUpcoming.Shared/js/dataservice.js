1bf75e30245a1627c0889e9a4147681d03f620d3
Andrew Hay <andrewcameronhay@hotmail.com>
2014-12-04 22:23:38 -0800

    initial src commit

---
+++ src/PortlandUpcoming.Shared/js/dataservice.js
@@ -0,0 +1,45 @@
+﻿(function () {
+
+    function get(url) {
+
+        // TODO: add timeout, retry and error handling
+
+        var url = 'http://padnug.org/api/upcoming';
+
+        return WinJS.xhr({
+            url: url,
+            responseType: 'json'
+        });
+
+    }
+
+    WinJS.Namespace.define("DataService", {
+        getEvents: function () {
+
+            return get('http://padnug.org/api/upcoming').then(function (result) {
+
+                if (!result || result.length < 1)
+                    return [];
+
+                var list = JSON.parse(result.response);
+
+                if (!list || list.length < 1)
+                    return [];
+
+                for (var i = 0; i < list.length; i++) {
+
+                    var item = list[i];
+
+                    if (item.Speakers && item.Speakers.length > 0) {
+                        item.firstSpeaker = item.Speakers[0];
+                        item.firstSpeaker.formattedImageUrl = 'http://padnug.org' + item.firstSpeaker.ImageUrl;
+                    }
+                }
+
+                return list;
+
+            });
+        }
+    });
+
+})();
---
+++ src/PortlandUpcoming.Shared/pages/home/home.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+
+        ready: function (element, options) {
+
+            DataService.getEvents().then(function (list) {
+
+                var bindingList = new WinJS.Binding.List(list);
+                eventListView.winControl.itemDataSource = bindingList.dataSource;
+
+            });
+        }
+    });
+})();
