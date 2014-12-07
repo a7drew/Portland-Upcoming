c2cb2637b7e765c26e308c2dfad7ab5368f1d922
Andrew Hay <andrewcameronhay@hotmail.com>
2014-12-06 20:49:36 -0800

    databound oniteminvoked event to view model in home.html

--- src/PortlandUpcoming.Shared/pages/home/homeViewModel.js
+++ src/PortlandUpcoming.Shared/pages/home/homeViewModel.js
@@ -1,26 +1,41 @@
 ﻿(function () {
 
-    var homeViewModel = WinJS.Class.define(
+    var vm = WinJS.Class.define(
         function () {
             // c'tor
+            this.start = this.start.bind(this);
+            this.itemInvoked = WinJS.Utilities.markSupportedForProcessing(this.itemInvoked.bind(this));
         },
         {
             // instance members
 
             itemDataSource: {
                 get: function () {
-                    return _itemDataSource;
+                    return this._itemDataSource;
                 }
             },
 
-            start: function() {
+            start: function () {
+
+                var that = this;
 
                 return DataService.getEvents().then(function (list) {
 
+                    that._list = list;
+
                     var bindingList = new WinJS.Binding.List(list);
-                    this._itemDataSource = bindingList.dataSource;
+                    that._itemDataSource = bindingList.dataSource;
 
                 });
+            },
+
+            itemInvoked: function (e) {
+
+                var item = this._list[e.detail.itemIndex];
+                var options = { item: item };
+
+                WinJS.Navigation.navigate("/pages/detail/detail.html", options)
+
             }
         },
         {
@@ -29,7 +44,7 @@
     );
 
     WinJS.Namespace.define('PortlandUpcoming', {
-        HomeViewModel : homeViewModel
+        HomeViewModel: vm
     });
 
 })();
