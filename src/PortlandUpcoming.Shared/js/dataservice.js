088a6090be13b783761e322bd3641d5adb62b2d2
Andrew Hay <andrewcameronhay@hotmail.com>
2014-12-07 20:18:18 -0800

    added formatted date field to home and detail

--- src/PortlandUpcoming.Shared/js/dataservice.js
+++ src/PortlandUpcoming.Shared/js/dataservice.js
@@ -13,6 +13,40 @@
 
     }
 
+    function pad(n, width, z) {
+        z = z || '0';
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
+
+    function formatItem(item) {
+
+        if (item.Speakers && item.Speakers.length > 0) {
+            item.firstSpeaker = item.Speakers[0];
+            item.firstSpeaker.formattedImageUrl = 'http://padnug.org' + item.firstSpeaker.ImageUrl;
+        }
+
+        var d = item.Date.replace(/[^0-9 +]/g, '');
+        d = new Date(parseInt(d));
+
+        var h = d.getHours() + 3;
+        var m = d.getMinutes();
+
+        var amPM = (h > 11) ? 'PM' : 'AM';
+
+        if (h > 12)
+            h -= 12;
+
+        var t = h + ':' + pad(m, 2) + ' ' + amPM;
+
+        item.formattedDate = d.toDateString() + ', ' + t;
+
+        item.Location.formattedCityStateZipCode =
+            item.Location.City + ', ' +
+            item.Location.State + ' ' +
+            item.Location.ZipCode;
+    }
+
     WinJS.Namespace.define("DataService", {
         getEvents: function () {
 
@@ -26,23 +60,8 @@
                 if (!list || list.length < 1)
                     return [];
 
-                for (var i = 0; i < list.length; i++) {
-
-                    var item = list[i];
-
-                    if (item.Speakers && item.Speakers.length > 0) {
-                        item.firstSpeaker = item.Speakers[0];
-                        item.firstSpeaker.formattedImageUrl = 'http://padnug.org' + item.firstSpeaker.ImageUrl;
-                    }
-
-                    var d = item.Date.replace(/[^0-9 +]/g, '');
-                    item.formattedDate = new Date(parseInt(d));
-
-                    item.Location.formattedCityStateZipCode =
-                        item.Location.City + ', ' +
-                        item.Location.State + ' ' +
-                        item.Location.ZipCode;
-                }
+                for (var i = 0; i < list.length; i++)
+                    formatItem(list[i]);
 
                 return list;
 
---
+++ src/PortlandUpcoming.Shared/js/helpers.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    Date.prototype.getMonthName = function () {
+        return months[this.getMonth()];
+    };
+
+    Date.prototype.getDayName = function () {
+        return days[this.getDay()];
+    };
+
+    Date.prototype.getFormattedTime = function () {
+
+        var hours = this.getHours();
+        var isPM = hours > 11;
+        if (hours > 12)
+            hours -= 12
+        var min = this.getMinutes();
+        if (min < 10)
+            min = '0' + min;
+
+        return hours + ':' + min;
+    };
+
+})();
